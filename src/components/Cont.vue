87d11f85c1a73d54d04b756a4fc02773f673f06e
Minos1329S <stas.lom06@gmail.com>
2023-03-08 17:50:47 +0200

    Add new element

---
+++ src/components/Cont.vue
@@ -0,0 +1,40 @@
+<script setup>
+import TimerSandEmptyIcon from "vue-material-design-icons/TimerSandEmpty.vue";
+</script>
+
+<template>
+  <div class="board">
+    <div class="wrapper">
+      <TimerSandEmptyIcon :size="186" fillColor="#ADADAD"></TimerSandEmptyIcon>
+      <p>Текст</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {TimerSandEmptyIcon}
+  }
+</script>
+
+<style scoped>
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+p {
+  font-size: 20px;
+  color:#999;
+  margin-top: 70px;
+}
+
+.board {
+  width: 885px;
+  height: 466px;
+  border: 0.5px solid rgba(0, 0, 0, 0.2);
+}
+
+</style>
